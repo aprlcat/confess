d0527a95ce698e49866c804d6ad32439e5693468
batteredbunny <uwu@catnip.ee>
2024-07-06 20:45:32 +0300

    Bump deps

--- build.nix
+++ build.nix
@@ -3,7 +3,7 @@ buildGoModule {
   src = ./.;
 
   name = "confess";
-  vendorHash = "sha256-ezi9LJCChoyianR5iI27arRnsy8jM2EI8koQQCFAAWM=";
+  vendorHash = "sha256-C4S7yxl04szbNI+Ue0c4qxH4gLeqPA8kpomMDDytDOY=";
 
   ldflags = [
     "-s"
